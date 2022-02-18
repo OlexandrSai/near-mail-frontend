f4cb87a6becac8a36f39df5e771d1096b15bfaaa
mihail.shevchuk <mihail.s@hubx.capital>
2022-02-18 18:13:33 +0200

    NG | Search done

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MailService} from "../../services/mail.service";
 
 @Component({
@@ -8,7 +8,12 @@ import {MailService} from "../../services/mail.service";
 })
 export class DashboardComponent implements OnInit {
   public modalOpen = false;
-  constructor(public mailService: MailService) { }
+  public search = '';
+  public messages: any[] = [];
+  public searchableColumns = ['message', 'sender'];
+
+  constructor(public mailService: MailService) {
+  }
 
   ngOnInit(): void {
     this.loadData();
@@ -16,5 +21,38 @@ export class DashboardComponent implements OnInit {
 
   async loadData() {
     await this.mailService.loadMessages();
+    this.messages = this.mailService.myMessages;
+  }
+
+  async searchChange() {
+    this.messages = this.mailService.myMessages;
+
+    if (this.messages && this.search !== '') {
+      let highlightData = this.messages.map((message) => {
+        const highlightText: any = {};
+        this.searchableColumns.map((keyColumn) => {
+          highlightText[keyColumn] = message?.[keyColumn].replace(
+            new RegExp(
+              this.search.replace(/[*[&<$.|^>\\/\]"?()+]/g, (s) => {
+                return '\\' + s;
+              }),
+              'gi'
+            ),
+            (str: any) => {
+              return str ? `${'<mark>' + str + '</mark>'}` : str;
+            }
+          );
+        });
+        return {...message, ...highlightText};
+      });
+
+      if (this.search) {
+        highlightData = highlightData.filter((message, id) =>
+          this.searchableColumns.some((keyColumn) => message[keyColumn] !== this.messages[id][keyColumn])
+        );
+      }
+
+      this.messages = highlightData;
+    }
   }
 }
